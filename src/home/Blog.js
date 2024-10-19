866beb3f26d3c51d504f3eb77e58176d777f989f
bieun <triwon20@gmail.com>
2024-10-19 21:38:00 +0900

    icon_box start

--- src/home/Blog.js
+++ src/home/Blog.js
@@ -1,8 +1,19 @@
+const iconbox_style = {
+  width: '1240px',
+  overflow: 'hidden',
+  margin: '10px auto',
+  backgroundColor: 'pink',
+  height: '500px'
+};
+
 const element = () => {
   return (
     <>
       <div id="icon_title">
         "WHAT'S FOR DINNER"<br/><span>FINALLY HAS AN<br/>EASY ANWSER</span>
+      </div>
+      <div id='icon_box' style={iconbox_style}>
+
       </div>
     </>
   );
