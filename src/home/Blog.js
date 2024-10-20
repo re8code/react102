434d8c8e64b35bea902bd474f09755ac8a550e13
bieun <triwon20@gmail.com>
2024-10-20 17:33:36 +0900

    icon_box clear

--- src/home/Blog.js
+++ src/home/Blog.js
@@ -1,19 +1,22 @@
+import Panel from './Panel.js';
+
 const iconbox_style = {
   width: '1240px',
   overflow: 'hidden',
-  margin: '10px auto',
-  backgroundColor: 'pink',
-  height: '500px'
+  margin: '10px auto'
 };
 
 const element = () => {
+  let box = [];
+  for (let i=1; i<5; i++) box.push(<Panel index={i}/>);
+
   return (
     <>
       <div id="icon_title">
         "WHAT'S FOR DINNER"<br/><span>FINALLY HAS AN<br/>EASY ANWSER</span>
       </div>
       <div id='icon_box' style={iconbox_style}>
-
+        {box}
       </div>
     </>
   );
---
+++ src/home/Panel.js
@@ -0,0 +1,14 @@
+import './home.css';
+
+const element = (props) => {
+  let path = `./icon${props.index}.png`;
+  return (
+    <p>
+      <img src={path} alt="icon1 image" /><br />
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua.
+    </p>
+  );
+}
+
+export default element;
